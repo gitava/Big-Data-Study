1253b09b03a2af356007f0a304b6aa633d3469df
gitava <goodysharp@gmail.com>
2020-06-24 19:00:42 +0800

    * changed the structure a bit   * add a few static method.
    * add feature to find the PATH_BASE automatically.

--- Hadoop The Definitive Guide 4th Edition/ch02-mr-intro/src/main/java/HDFSFileIfExist.java
+++ Hadoop The Definitive Guide 4th Edition/ch02-mr-intro/src/main/java/HDFSFileIfExist.java
@@ -16,7 +16,28 @@ import com.sun.org.apache.bcel.internal.generic.NEW;
  */
 public class HDFSFileIfExist {
 
-	private final static String PATH_BASE = "hdfs://192.168.33.101:9000";
+	//Set a default path
+	private static String path_base = "hdfs://192.168.33.101:9000";
+	private static final String HDFS_IMPL="org.apache.hadoop.hdfs.DistributedFileSystem";
+
+	public static String getPathBase() {
+		return path_base;
+	}
+
+	public static void setPathBase(String pathBase) {
+		path_base = pathBase;
+	}
+
+	private static String parsePathBase(String path) {
+		int start = path.indexOf("//");
+		int end = path.indexOf("/", start + 2);
+
+//		System.out.println("start: " + start);
+//		System.out.println("end: " + end);
+//		System.out.println(path);
+
+		return path.substring(0, end);
+	}
 
 	public static void main(String[] args) {
 
@@ -27,22 +48,27 @@ public class HDFSFileIfExist {
 			if (args[0].substring(0, 4).toLowerCase().equals(new String("hdfs"))) {
 				fileName = args[0];
 				System.out.println("Absolute path is provided.");
+
+				// To get the BASE part of the path
+				setPathBase(parsePathBase(args[0]));
+				System.out.println("PATH_BASE: " + path_base);
+
 			} else {
-				fileName = PATH_BASE + "/" + args[0];
+				fileName = path_base + "/" + args[0];
 				System.out.println("Relative path is provided.");
 
 			}
 
 		} else {
 			System.out.println("No path is provided.");
-			fileName = PATH_BASE + "/" + "output";
+			fileName = path_base + "/" + "output";
 		}
 
 		try {
 			// 加载配置项
 			Configuration conf = new Configuration();
-			conf.set("fs.defaultFS", PATH_BASE);
-			conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+			conf.set("fs.defaultFS", getPathBase());
+			conf.set("fs.hdfs.impl", HDFS_IMPL);
 
 			// 创建文件系统实例
 			FileSystem fs = FileSystem.get(conf);
@@ -51,7 +77,7 @@ public class HDFSFileIfExist {
 //			String fileName = "文件或目录名";
 			Path file = new Path(fileName);
 //			Path file = new Path("hdfs://192.168.33.101:9000/output/part-r-00000");
-			System.out.println("file: " + file);
+//			System.out.println("file: " + file);
 
 			// 判断文件是否存在
 			if (fs.exists(file)) {
@@ -67,6 +93,7 @@ public class HDFSFileIfExist {
 			e.printStackTrace();
 		}
 	}
+
 }
 //Based on:
 //————————————————
